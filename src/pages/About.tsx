d2e4b24eb854800355a1dbbdaa8ff9672afb7da9
Dimas NP <dimsdeall@gmail.com>
2024-08-25 22:06:11 +0700

    fix: deleta unused code.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,12 +2,11 @@ import { useCallback, useEffect, useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "../App.css";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function About() {
   const [count, setCount] = useState(0);
   const navigate = useNavigate();
-  const params = useParams()
   const location = useLocation();
   const [isConfirmed, setIsConfirmed] = useState(false);
   const handleBackButton = useCallback((event: PopStateEvent) => {
